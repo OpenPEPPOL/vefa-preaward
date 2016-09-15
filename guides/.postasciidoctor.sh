9259b5cce0e994754bda594f48e736218f45c44c
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-09-15 16:28:11 +0200

    Updated paths.

--- guides/.postasciidoctor.sh
+++ guides/.postasciidoctor.sh
@@ -2,4 +2,6 @@
 
 mkdir /target/esens/guide
 mv /target/esens/bis47x /target/esens/bis54 /target/esens/bis54x /target/security /target/esens/guide/
-mv /target/esens/bisTendering /target/esens/guide/tendering
+
+mkdir -p /target/peppol/guide
+mv /target/esens/bisTendering /target/peppol/guide/tendering
